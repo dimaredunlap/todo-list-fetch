bbdf0f14ee5a7b9c9a23188a9cc4ab6260a24a4f
dimaredunlap <dimaredunlap@gmail.com>
2022-07-12 22:59:14 +0000

    starting project

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,26 +1,76 @@
-import React from "react";
+import React, {useState} from "react";
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 const Home = () => {
-	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+	const [ToDoList, setToDoList] = useState([
+		"Do Homework",
+		"Do Laundry",
+		"Walk the Dog",
+	  ]);
+	
+	  const addItem = (onKeyDownEvent) => {
+		if (onKeyDownEvent.keyCode === 13) {
+		  let newTask = onKeyDownEvent.target.value;
+		  const newList = [...ToDoList, newTask];
+		  setToDoList(newList);
+		  setInputValue("");
+		}
+	  };
+	  const removeItem = (index) => {
+		const removeTask = ToDoList.filter((item, i) => i != index);
+		setToDoList(removeTask);
+	  };
+	  const [inputValue, setInputValue] = useState("");
+	  const [isShown, setIsShown] = useState({
+		state: false,
+		index: 0,
+	  });
+	  const list = ToDoList.map((item, i) => {
+		return (
+		  <div className="repeating" key={i}>
+			<li
+			  onMouseEnter={() => setIsShown({ state: true, index: i })}
+			  onMouseLeave={() => setIsShown({ state: false, index: 0 })}
+			>
+			  {item}{" "}
+			  {isShown.state === true && isShown.index === i ? (
+				<button onClick={() => removeItem(i)}>X</button>
+			  ) : (
+				""
+			  )}
+			</li>
+		  </div>
+		);
+	  });
+	
+	  return (
+		<div className="container-fluid card">
+		  <h1>ToDos</h1>
+		  <input
+			type="text"
+			onKeyDown={addItem}
+			placeholder=" "
+			id="fname"
+			name="fname"
+			onChange={e => setInputValue(e.target.value)}
+			value={inputValue}
+		  />
+		  <div>
+			<ul>
+			  {list}
+			  <li>
+				{ToDoList.length > 1
+				  ? `${ToDoList.length} items`
+				  : ToDoList.length > 0
+				  ? `${ToDoList.length} item`
+				  : "No tasks, add a task"}
+			  </li>
+			</ul>
+		  </div>
 		</div>
-	);
-};
-
+	  );
+	};
 export default Home;
